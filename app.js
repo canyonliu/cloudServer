ad5861d594d8536155906c1b158e61e5f9460722
liuqingcan <liuqingcan@baidu.com>
2025-10-28 15:15:33 +0800

    自动化部署 实验

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! you and me, first meeting');
+  res.send('Hello, World! you and me, first meeting， 自动化部署代码实验');
 });
 
 app.listen(port, () => {
